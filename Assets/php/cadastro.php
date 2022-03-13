d001ab7e5bf968dfcca57c689eebba16b14e7a01
VICENTE <64757262+sombo20@users.noreply.github.com>
2022-03-13 15:05:38 +0100

    Delete Assets/php directory

--- Assets/php/cadastro.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once("./Assets/classes/class.usuario.php");
-		if(isset($_POST["cadastrar"])){
-			//$cadastrar = new conexao();
-			$cadastrar = new usuario();
-			$cadastrar->cadastrar($_POST["name"],$_POST["password"]);
-		}
-?>
--- Assets/php/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-require("../classes/funcao.php");
- unset($_SESSION["user"]);
- header('Refresh: 0; URL = http://localhost:8080/CHAT-REAL-TIME/');
-?>
